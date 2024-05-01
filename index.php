fa0350f0d66897527b0ac23e111d1e5427416f56
dini restiani <drestiani45@gmail.com>
2024-05-01 14:25:21 +0700

    tugas oop dini

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
 
-require_once "Frog.php";
-require_once "Ape.php";
+require_once 'Frog.php';
+require_once 'Ape.php';
+require_once 'Animal.php';
+
+$sheep = new Animal("Shaun");
+$kodok = new kodok("buduk"); 
+$sungokong = new sungokong("kera sakti"); 
+
+echo "Name: " . $sheep->name . "<br>"; 
+echo "legs: " . $sheep->legs . "<br>"; 
+echo "cold blooded: " . ($sheep->cold_blooded) . "<br>";
+echo "<br>";
+
+echo "Name: " . $kodok->name . "<br>"; 
+echo "legs: " . $kodok->legs . "<br>"; 
+echo "cold blooded: " . ($kodok->cold_blooded) . "<br>"; 
+echo "Jump: " . $kodok->jump() . "<br>"; 
+echo "<br>";
+
+echo "Name: " . $sungokong->name . "<br>"; 
+echo "legs: " . $sungokong->legs . "<br>"; 
+echo "cold blooded: " . ($sungokong->cold_blooded) . "<br>"; 
+echo "Yell: " . $sungokong->yell() . "<br>"; 
+echo "<br>";
 
 ?>
